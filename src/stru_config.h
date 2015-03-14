45c71585ac7050c362b572c02a26d894d47a887a
eminom <eminem7409@163.com>
2015-03-15 02:33:27 +0800

    fixed

--- src/stru_config.h
+++ src/stru_config.h
@@ -2,8 +2,8 @@
 #ifndef _STRU_CONFIG__DEF__
 #define _STRU_CONFIG__DEF__
 
-//#define DBG(...)
-#define DBG(...)  { fprintf(stderr, __VA_ARGS__); fprintf(stderr,"\n");}
+#define DBG(...)
+//#define DBG(...)  { fprintf(stderr, __VA_ARGS__); fprintf(stderr,"\n");}
 
 
 #endif
--- test.rb
+++ test.rb
@@ -2,41 +2,55 @@
 # manually installed on my Ubuntu-10.04
 
 # config:
-target_name = 'EsNode'
 
-def node_path_finder
-	n_path = `which node`
-	if n_path.size == 0 then
-		print "node is not installed\n"
-		exit
+module IGen
+	def self.node_path_finder
+		n_path = `which node`
+		if n_path.size == 0 then
+			print "node is not installed\n"
+			exit
+		end
+		n_path.chomp
 	end
-	n_path.chomp
-end
 
+	def self.init
+		@target_name = 'EsNode'
+		@node_path = node_path_finder
+		@script_path = File.absolute_path(File.dirname(__FILE__))
+		@input_path = "#{@script_path}/.input/#{@target_name}.stru"
+		@output_path = "#{@script_path}/#{@target_name}.json"
+		@parser_path = "#{@script_path}/parser"
+		@output_inc  = "#{@script_path}/#{@target_name}.inc"
+	end
 
-# main:
-node_path = node_path_finder
-script_path = File.absolute_path(File.dirname(__FILE__))
-input_path = "#{script_path}/.input/#{target_name}.stru"
-output_path = "#{script_path}/#{target_name}.json"
-parser_path = "#{script_path}/parser"
-output_inc  = "#{script_path}/#{target_name}.inc"
+	def self.export_json
+		#print @input_path,"\n"
+		#print @output_path, "\n"
+		return if File.mtime(@input_path) <= File.mtime(@output_path)
+		cmd = "#{@parser_path} < #{@input_path} > #{@output_path}"
+		if true != system(cmd) then
+			print cmd, "\n"
+			print $?, "\n"
+			exit
+		end
+		print "#{@output_path} is updated.\n"
+	end
 
-cmd = "#{parser_path} < #{input_path} > #{output_path}"
-if true != system(cmd) then
-	print cmd, "\n"
-	print $?, "\n"
-	exit
-end
+	def self.export_inc
+		return if File.mtime(@output_path) <= File.mtime(@output_inc)
+		makejs_path = "#{@script_path}/js/make.js"
+		cmd = 
+		  "#{@node_path} \"#{makejs_path}\" \"#{@output_path}\">\"#{@output_inc}\""
+		# print cmd,"\n"
+		if true != system(cmd) then
+			print cmd,"\n"
+			print $?, "\n"
+			exit
+		end
+		print "#{@output_inc} is generated.\n"
+	end
+end  #end of module
 
-makejs_path = script_path + "/js/make.js"
-cmd = "#{node_path} \"#{makejs_path}\" \"#{output_path}\" >  #{output_inc}"
-print cmd,"\n"
-if true != system(cmd) then
-	print cmd,"\n"
-	print $?, "\n"
-	exit
-end
-
-print "Finished\n"
-
+IGen.init
+IGen.export_json
+IGen.export_inc

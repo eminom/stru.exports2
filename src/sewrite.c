ac29a8327727a5e928dee81effc2c2066df93284
home <hai.bai.cn@gmail.com>
2015-03-21 00:59:43 +0800

    fix impls

--- src/sewrite.c
+++ src/sewrite.c
@@ -9,6 +9,7 @@
 
 extern char* strdup(const char*);
 
+ParamDefault *curParamDefault = 0;
 ParamLink* curParam = 0;
 StruLink* curStru = 0;
 StaticLink *curStaticLink = 0;
@@ -25,12 +26,13 @@ char *curVarType = 0;
 #define DisposeStr(a)	\
 	if(a){free(a); a = 0;}
 
-ParamLink* se_createParamLink(const char *type, VarType varType, ParamLink *previous){
+ParamLink* se_createParamLink(const char *type, VarType varType, ParamDefault *paramDefault, ParamLink *previous){
 	ParamLink *rv = (ParamLink*)malloc(sizeof(ParamLink));
 	memset(rv, 0, sizeof(*rv));
 
 	rv->typeName = strdup(type);
 	rv->vt = varType;
+	rv->param_default = paramDefault;
 
 	rv->next = previous;
 	return rv;
@@ -61,8 +63,23 @@ StruLink* se_createStruLink(const char *name, const char*org, const char*meta, c
 	return rv;
 }
 
+ParamDefault* se_createParamDefault(VarDefault dt, const char *param_in) {
+	ParamDefault *pd = (ParamDefault*)malloc(sizeof(ParamDefault));
+	memset(pd, 0, sizeof(*pd));
+	pd->param_in = strdup(param_in);
+	pd->var_default = dt;
+	return pd;
+}
+
+void se_disposeParamDefault(ParamDefault *now){
+	if(!now){return;}
+	DisposeStr(now->param_in);
+	free(now);
+}
+
 void se_disposeParamLink(ParamLink *now){
 	if(!now){return;}
+	se_disposeParamDefault(now->param_default);
 	DisposeStr(now->typeName)
 
 	se_disposeParamLink(now->next);
@@ -119,15 +136,51 @@ cJSON* getType(VarType vt){
 	}
 }
 
-
-void _se_writeParamLinkRun(cJSON *ar, ParamLink *now){
+void _se_writeParamDefault(cJSON *obj, const char *type, ParamDefault *now, const char* methodName){
 	if(!now){return;}
-	_se_writeParamLinkRun(ar, now->next);
+	//Checking...
+	switch(now->var_default){
+	case dt_Integer:
+		if(strcmp(type, "int")){
+			fprintf(stderr, "Type mismatched from \"%s\" to dt_Integer in %s\n", type, methodName);
+			abort();
+		}
+		break;
+	case dt_Boolean:
+		if(strcmp(type, "bool")){
+			fprintf(stderr, "Type mismatched from \"%s\" to dt_Boolean in %s\n", type, methodName);
+			abort();
+		}
+		break;
+	case dt_Float:
+		if(strcmp(type, "float")){
+			fprintf(stderr, "Type mismatched from \"%s\" to dt_Float in %s\n", type, methodName);
+			abort();
+		}
+		break;
+	case dt_String:
+		if(strcmp(type, "const char*") && strcmp(type, "char*")){
+			fprintf(stderr, "Type mismatched from \"%s\" to dt_String in %s\n", type, methodName);
+			abort();
+		}
+		break;
+	default:
+		fprintf(stderr, "ERROR Var default :%d in %s\n", now->var_default, methodName);
+		abort();
+		break;
+	}
+	cJSON_AddItemToObject(obj, "default", cJSON_CreateString(now->param_in));
+}
+
+
+void _se_writeParamLinkRun(cJSON *ar, ParamLink *now, const char *methodName){
+	if(!now){return;}
+	_se_writeParamLinkRun(ar, now->next, methodName);
 
 	cJSON *o = cJSON_CreateObject();
 	cJSON_AddItemToObject(o, "isClass", getType(now->vt));
 	cJSON_AddItemToObject(o, "type", cJSON_CreateString(now->typeName));
-
+	_se_writeParamDefault(o, now->typeName, now->param_default, methodName);
 	//
 	cJSON_AddItemToArray(ar, o);
 }
@@ -141,7 +194,7 @@ void _se_writeMethodLinkRun(cJSON *ar, MethodLink *now){
 	cJSON_AddItemToObject(o, "return", cJSON_CreateString(now->returnTypeName));
 	cJSON_AddItemToObject(o, "call", cJSON_CreateString(now->methodName));
 	cJSON *paramArray = cJSON_CreateArray();
-	_se_writeParamLinkRun(paramArray, now->params);
+	_se_writeParamLinkRun(paramArray, now->params, now->methodName);
 	cJSON_AddItemToObject(o, "params", paramArray);
 
 	//
@@ -194,7 +247,7 @@ void _se_writeStaticLinkRun(cJSON *ar, StaticLink *now){
 	cJSON_AddItemToObject(o, "name", cJSON_CreateString(now->name));
 	cJSON_AddItemToObject(o, "type", cJSON_CreateString(now->returnType));
 	cJSON *mtAr = cJSON_CreateArray();
-	_se_writeParamLinkRun(mtAr, now->params);
+	_se_writeParamLinkRun(mtAr, now->params, now->name);
 	cJSON_AddItemToObject(o, "params", mtAr);
 	cJSON_AddItemToArray(ar, o);
 }
